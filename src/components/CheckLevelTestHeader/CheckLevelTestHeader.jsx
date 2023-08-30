a3dfce2656494181ee08275b081024ffc17e2198
Salikhovakbar <pcfjkjhkk@gmail.com>
2023-08-30 13:42:31 +0500

    change

--- src/components/CheckLevelTestHeader/CheckLevelTestHeader.jsx
+++ src/components/CheckLevelTestHeader/CheckLevelTestHeader.jsx
@@ -4,23 +4,47 @@ import logo from '../../images/black-logo.png'
 import { Link } from 'react-router-dom'
 import { AiOutlineMenu } from 'react-icons/ai'
 const CheckLevelTestHeader = () => {
-    const headerLiOptions = [
+    const headerLiOptions = {
+        sections: [
         'Grammar',
-        'Reading'
-    ]
+        'Reading',
+        'Vocabulary'
+    ],
+        levels: [
+            'Elementary',
+            "Pre-intermediate",
+            "Intermediate"
+        ]
+}
     const [sidebarBoxStatement, setSidebarBoxStatement] = useState(false)
   return (
     <header className={c.header_check_level}>
+<div className={c.header_check_level_box}>
 <div className={c.header_logo_box}>
     <img src={logo} alt="logo" />
 </div>
 <button className={c.header_sidebar_btn} onClick={() => {
-    setSidebarBoxStatement(true)
+    setSidebarBoxStatement(!sidebarBoxStatement)
 }}>
     <AiOutlineMenu/>
 </button>
+</div>
 <div style={sidebarBoxStatement ? {top: '0'} : null} className={c.sidebar_box}>
-
+<ul className={c.sidebar_ul_options}>
+    {
+        headerLiOptions.sections.map((e, index) => 
+        <li key={index}>{e}
+        <div className={c.sidebar_select_box}>
+            {
+                headerLiOptions.levels.map((a, i) =>
+                <div key={i}>{a}</div>
+                )
+            }
+        </div>
+        </li>
+        )
+    }
+</ul>
 </div>
     </header>
   )
--- src/routes/CheckLevelTest/CheckLevelTest.jsx
+++ src/routes/CheckLevelTest/CheckLevelTest.jsx
@@ -1,10 +1,34 @@
-import React from 'react'
 import CheckLevelTestHeader from '../../components/CheckLevelTestHeader/CheckLevelTestHeader'
-
+import {useState,useEffect} from 'react'
+import { useLocation } from 'react-router-dom'
+import logo from '../../images/logo-nextgen.png'
 const CheckLevelTest = () => {
+  const hosting = 'http://localhost:5000'
+  const [testData, setTestData] = useState()
+  const { pathname } = useLocation()
+  useEffect(() => {
+;(async () => {
+try{
+  const response = await fetch(`${hosting}/test`,{
+    method: 'GET',
+    headers: {
+      token: localStorage.getItem('token')
+    }
+  })
+  const data = await response.json()
+  if(data.status === 404) throw new Error(data.error)
+  else if(data.status === 200){
+  setTestData(data.data)
+  }
+}catch(err){
+  alert(err.message)
+}
+})()
+  }, [])
   return (
     <>
     <CheckLevelTestHeader/>
+    {pathname === '/check-level-test'? <div style={{width: '100%', height: '90vh'}}><img style={{width: '100%', height: '100%', objectFit: 'contain'}} src={logo} alt="" /></div> : null}
     </>
   )
 }
